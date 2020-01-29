b037c5b4aa46bd6bf46dfef5239d48ae000b0fe0
clooner8133 <clooner8133@gmail.com>
2020-01-29 19:32:14 +0330

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace farshid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
